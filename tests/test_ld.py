bd578c5eaf17ffef3a031e2142a4c0a704045df5
Jeremy McRae <jmcrae@illumina.com>
2019-03-08 14:05:25 -0800

    added test for variant with low D prime

--- tests/test_ld.py
+++ tests/test_ld.py
@@ -15,3 +15,21 @@ class TestLDEstimator(unittest.TestCase):
         ld = estimate_ld(var1, var2, is_haploid)
         self.assertEqual(ld.dprime, 0.9999999995259259)
         self.assertEqual(ld.r_squared, 0.7714285713447618)
+    
+    def test_estimate_ld_low_dprime(self):
+        ''' check estimate_ld works correctly for variants with low D'
+        '''
+        var1 = [('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','G'),
+            ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','G'),
+            ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','A'), ('G','G'),
+            ('G','A'), ('G','G'), ('G','G'), ('G','G'), ('G','G')]
+
+        var2 = [('G','G'), ('G','G'), ('A','G'), ('G','G'), ('G','G'), ('G','G'),
+            ('G','A'), ('G','G'), ('G','G'), ('G','G'), ('A','G'), ('G','G'),
+            ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('A','G'), ('G','G'),
+            ('G','G'), ('G','G'), ('G','G'), ('G','G'), ('G','A')]
+
+        is_haploid = [False] * len(var1)
+        ld = estimate_ld(var1, var2, is_haploid)
+        self.assertEqual(ld.dprime, 0.37448437109790317)
+        self.assertEqual(ld.r_squared, 0.05227073010963888)
