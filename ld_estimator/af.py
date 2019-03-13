e773f5453872718f02ef606003fef9ee9277d9fa
Jeremy McRae <jmcrae@illumina.com>
2019-03-12 17:45:11 -0700

    removed outdated python code

--- ld_estimator/af.py
+++
@@ -1,9 +0,0 @@
-
-def get_allele_freqs(known, unknown):
-    ''' calculate allele frequencies
-    '''
-    chroms = (known.aa + known.ab + known.ba + known.bb + (2 * unknown))
-    pA1 = (known.aa + known.ab + unknown) / chroms
-    pA2 = (known.aa + known.ba + unknown) / chroms
-
-    return pA1, 1.0 - pA1, pA2, 1.0 - pA2
