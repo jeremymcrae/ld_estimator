edb85bcba4474ca48a691f72199fb09271c75659
Jeremy McRae <jmcrae@illumina.com>
2019-05-02 13:25:24 -0700

    now can check multiple positions at once with site_ld

--- ld_estimator/site.py
+++ ld_estimator/site.py
@@ -3,39 +3,47 @@
 
 from ld_estimator.utils import prepare_vcf, check_ld
 
-def site_ld(vcf, chrom, pos, window=100000, subset=None):
-    ''' calculate LD at one site to all surrounding variants
+def site_ld(vcf, chrom, positions, window=100000, subset=None):
+    ''' calculate LD at specified site/s to all surrounding variants
 
     Args:
         vcf: path to vcf, or pysam.VariantFile object
         chrom: chromosome.
-        pos: start of region to calculate LD within
+        positions: position/s of variant/s to calculate LD against.
         window: number of base pairs to check LD within
         subset: list of sample IDs in VCF to include in LD calculation. Default
             is to include every sample.
 
     Returns:
         list of LD estimates, each as a dict with [chrom, var1_pos, var2_pos,
-            r_squared and dprime] keys.
+            var1_id, var2_id, r_squared, dprime and phase] keys.
     '''
 
     vcf = prepare_vcf(vcf, subset)
 
-    variants = list(x for x in vcf.fetch(chrom, pos-1, pos) if x.pos == pos)
-    if variants == []:
-        raise ValueError(f'no variant found at: {chrom}:{pos}')
+    if not isinstance(positions, list):
+        positions = [positions]
 
-    var1 = variants[0]
-    var1_geno = [var1.samples[x].alleles for x in var1.samples]
+    variants = []
+    for pos in positions:
+        entries = list(x for x in vcf.fetch(chrom, pos-1, pos) if x.pos == pos)
+        if entries == []:
+            raise ValueError(f'no variant found at: {chrom}:{pos}')
+        var = entries[0]
+        geno = [var.samples[x].alleles for x in var.samples]
+        variants.append([var, geno])
 
     lds = []
-    for var2 in vcf.fetch(chrom, pos - window, pos + window):
-        if var1 == var2:
-            continue
-
+    for var2 in vcf.fetch(chrom, min(positions) - window, max(positions) + window):
         var2_geno = [var2.samples[x].alleles for x in var2.samples]
         ploidy = [False] * len(var2_geno)
-        data = check_ld(var1, var1_geno, var2, var2_geno, ploidy)
-        lds.append(data)
+        for var1, var1_geno in variants:
+            if var1 == var2:
+                continue
+            if abs(var1.pos - var2.pos) > window:
+                continue
+
+            data = check_ld(var1, var1_geno, var2, var2_geno, ploidy)
+            lds.append(data)
 
     return lds
